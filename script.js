4af9b8c46341e1fcb363809d1e71e9bb76045fcd
Artem Sokur <teebdol@bk.ru>
2026-02-03 04:55:09 +0400

    Завершено создание основных функций. Рисование одним цветом, ластик, радужный режим, очистка, и изменение количества пикселей

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
+// Основные элементы на странице
+
 const sheet = document.querySelector(".sheet");
-let selectColor = "lightpink";
+const solidColoringBtn = document.querySelector("#solid-coloring");
+const eraserBtn = document.querySelector("#eraser");
+const rainbowBtn = document.querySelector("#rainbow");
+const clearBtn = document.querySelector("#clear");
+const inputRange = document.querySelector("#input-range");
+const inputDisplay = document.querySelector("#input-display");
 
-let counter = 30;
-let color;
+// Параметры по умолчанию
+
+let selectedColor = "lightpink";
+let pixelsQuantity = 30;
 let isHoldMouse = false;
+let currentMode = "solidColoring";
+let pixels;
 
-let currentMode = "coloring";
+// Логика создания и перезаписи пикселей в зависимости от значения ползунка
 
-// Создание пикселей и добавление их на лист
-const createPixels = (counter) => {
+const createPixels = (pixelsQuantity) => {
+  sheet.style.setProperty("--pixels-quantity", pixelsQuantity);
   let pixel;
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < counter * counter; i++) {
+  for (let i = 0; i < pixelsQuantity * pixelsQuantity; i++) {
     pixel = document.createElement("div");
     pixel.classList.add("pixel");
-    // pixel.setAttribute("draggable", "false");
     fragment.append(pixel);
   }
   sheet.append(fragment);
+  pixels = document.querySelectorAll(".pixel");
 };
-createPixels(counter);
+createPixels(pixelsQuantity);
+
+const reCreatePixels = (pixelsQuantity) => {
+  sheet.innerHTML = "";
+  createPixels(pixelsQuantity);
+};
+
+// Слушатели
+
+// Количество пикселей
+
+inputRange.addEventListener("input", () => {
+  pixelsQuantity = inputRange.value;
+  inputDisplay.textContent = `${pixelsQuantity} x ${pixelsQuantity}`;
+});
+
+inputRange.addEventListener("change", () => {
+  reCreatePixels(pixelsQuantity);
+});
+
+// Переключение режимов
+
+solidColoringBtn.addEventListener("click", (event) => {
+  currentMode = "solidColoring";
+});
+
+eraserBtn.addEventListener("click", (event) => {
+  currentMode = "eraser";
+});
+
+rainbowBtn.addEventListener("click", (event) => {
+  currentMode = "rainbow";
+});
+
+clearBtn.addEventListener("click", (event) => {
+  pixels.forEach((pixel) => pixel.style.removeProperty("background-color"));
+});
 
 // Логика зажатой лкм
+
 window.addEventListener("mousedown", () => {
   isHoldMouse = true;
 });
@@ -29,7 +77,7 @@ window.addEventListener("mouseup", () => {
   isHoldMouse = false;
 });
 
-// Окрашивание пикселей в выбранный цвет при зажатой лкм
+// Логика рисования
 
 sheet.addEventListener("mousedown", (event) => {
   if (event.target === sheet) return;
@@ -42,9 +90,25 @@ sheet.addEventListener("mouseover", (event) => {
 });
 
 const coloring = (event) => {
-  if (currentMode === "coloring") {
-    event.target.style.backgroundColor = selectColor;
-  } else if (currentMode === "eraser") {
-    event.target.style.removeProperty("background-color");
+  switch (currentMode) {
+    case "solidColoring":
+      event.target.style.backgroundColor = selectedColor;
+      break;
+    case "eraser":
+      event.target.style.removeProperty("background-color");
+      break;
+    case "rainbow":
+      event.target.style.backgroundColor = getRandomRainbowColor();
+      break;
   }
+};
+
+// Логика получения ярких цветов для радужного режима
+
+const getRandomRainbowColor = () => {
+  let hue = Math.floor(Math.random() * 360);
+  let saturation = Math.floor(Math.random() * 11 + 90);
+  let lightness = Math.floor(Math.random() * 11) + 40;
+  let currentColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  return currentColor;
 };
